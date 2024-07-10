58904dab4031783635b94735dff609136c14f416
lsvishaal <lsvishaal7@gmail.com>
2024-07-10 17:11:17 +0530

    chore: Update page title, import statements, and add Tailwind CSS styled component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './App.jsx';
-import './index.css';
-
+import { Navbar, MyComponent } from './App.jsx';
+import './output.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <MyComponent />
   </React.StrictMode>
 );
